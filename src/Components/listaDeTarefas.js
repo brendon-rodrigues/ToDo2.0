0f6d072f9cb44314908802eb334f1c35558b2194
Brendon Rodrigues <rodriguesbrendon861@gmail.com>
2022-06-18 20:38:21 -0300

    botão adicionado

--- src/Components/listaDeTarefas.js
+++ src/Components/listaDeTarefas.js
@@ -1,11 +1,27 @@
 import React, {useState} from "react";
 import { FaPlus, FaTrash, FaCheck } from "react-icons/fa";
 import styled from "styled-components"
+const Container = styled.section`
+    display: flex;
+    justify-content: space-around;
+`
+const Form = styled.form`
+    width: 50%;
+    height: 50vh;
+    background: #32b9be;
+    margin: 10px;
+    padding: 15px;
+    input, label{
+        margin: 10px
+    }
+   
+`
 const ListBlock = styled.div`
-    width: 100%;
+    width: 50%;
     height: 100%;
   li {
     width: 90%;
+    height: 20vh;
     color: black;
     display: flex;
     justify-content: space-between;
@@ -24,9 +40,19 @@ const TrashButton = styled.button`
     width: 25px;
     height: 25px;
 `;
+const taskInput = ()=>{
+    return(
+        <>
+            <textarea></textarea>
+            <input type="date"/>
+
+        </>
+    )
+}
 export default function TaskList(){
     const [input, setInput] = useState();
     const [task, setTask] = useState([]);
+    const [inputli, setInputLi] = useState(false)
     const newTask = ()=>{
         if(!input){
             return alert("Nenhuma tarefa foi adicionada, escreva uma tarefa por favor")
@@ -43,9 +69,15 @@ export default function TaskList(){
        setTask(listFilter);
     };
     return(     
-        <>
-            <input name="input" value={input} onChange={e=>{setInput(e.target.value)}}  placeholder="adicionar tarefa"></input>
-            <button onClick={()=>{newTask()}}>Add</button>
+        <Container>
+            <Form onSubmit={e=>{
+                e.preventDefault()
+            }}>
+                <label>Adicionar Tarefa</label><br/>
+                <input name="input" value={input} onChange={e=>{setInput(e.target.value)}}  placeholder="adicionar tarefa"></input>
+                <button onClick={()=>{newTask()}}>Add</button>
+            </Form>
+            
             <ListBlock>
                 <ul>
                     {task.map((items) => (
@@ -56,11 +88,15 @@ export default function TaskList(){
                                 <TrashButton onClick={() => { delTask(items.id);}}>
                                     <FaTrash />
                                 </TrashButton>
+                                <button onClick={()=>{
+                                    setInputLi(!inputli)
+                                }}>+</button>
                             </div>
+                            {inputli && taskInput()}
                         </li>
                      ))}
                 </ul>
             </ListBlock>           
-        </>
+        </Container>
     );
 }
